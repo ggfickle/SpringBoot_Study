3ded1dc1f96db2fb9845556336e4027136d8a8c3
Desktop PC <841192493@qq.ccom>
2020-08-11 22:36:46 +0800

    add person.properties

--- spring-boot-02-config/src/main/java/com/hf/demo/bean/Person.java
+++ spring-boot-02-config/src/main/java/com/hf/demo/bean/Person.java
@@ -2,6 +2,7 @@ package com.hf.demo.bean;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,6 +21,7 @@ import java.util.Objects;
  *
  * @Value无法注入复杂类型   只能取出基本类型数据
  */
+@PropertySource(value="classpath:person.properties")
 @Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
